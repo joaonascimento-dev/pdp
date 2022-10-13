0707c3b7a1c6d9c0369ab4ecd439c4926825af83
João Nascimento <53279568+joaonascimento-dev@users.noreply.github.com>
2022-10-13 19:59:55 -0300

    trazendo vagas do banco
    bd: root root

--- pdp/src/main/java/br/fatec/b3comandas/controller/SiteController.java
+++ pdp/src/main/java/br/fatec/b3comandas/controller/SiteController.java
@@ -7,13 +7,18 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.fatec.b3comandas.filtro.UsuarioFiltro;
+import br.fatec.b3comandas.filtro.VagaFiltro;
 import br.fatec.b3comandas.model.Usuario;
+import br.fatec.b3comandas.model.Vaga;
 import br.fatec.b3comandas.service.UsuarioService;
+
+import br.fatec.b3comandas.service.VagaService;
 
 //@CrossOrigin(origins = "http://localhost:3000")
 @Controller
@@ -22,9 +27,15 @@ public class SiteController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @Autowired
+    private VagaService vagaService;
+
     @RequestMapping("/")
     public ModelAndView index(HttpSession session) {
         ModelAndView mav = new ModelAndView("index");
+
+        List<Vaga> listVaga = vagaService.findByCriteria((VagaFiltro) new VagaFiltro.Builder().limite(3).build());
+        mav.addObject("listVaga", listVaga);
 
         return mav;
     }
@@ -61,6 +72,27 @@ public class SiteController {
         session.setAttribute("usuario", usuario);
 
         return "redirect:/";
+    }
+
+    @RequestMapping("/vagas")
+    public ModelAndView vagas(HttpSession session) {
+        ModelAndView mav = new ModelAndView("vagas");
+
+        List<Vaga> listVaga = vagaService.findByCriteria((VagaFiltro) new VagaFiltro.Builder().build());
+        mav.addObject("listVaga", listVaga);
+
+        return mav;
+    }
+
+    @RequestMapping("/vaga/{id}")
+    public ModelAndView vaga(@PathVariable(name = "id") Integer id,
+            HttpSession session) {
+        ModelAndView mav = new ModelAndView("vagaDetalhe");
+
+        Vaga vaga = vagaService.findById(id);
+        mav.addObject("vaga", vaga);
+
+        return mav;
     }
 
 }
